449b9ef7cf976e0f9af0a966e5f578e298908c6f
Julian Porter <julian@porternet.org.uk>
2021-05-16 01:21:24 +0100

    Initial upload

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+    
+here = path.abspath(path.curdir)
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+scripts=[path.join(here,f'src/parse_flp') ]
+
+packages=find_packages('src')
+print(f'Packages is "{packages}"')
+
+setup(
+    name='FLPFile',
+    version='0.2.0',
+    description=f'A simple FL Studio Project File parser',
+    long_description=long_description,
+    author='Julian Porter',
+    author_email='julian@porternet.org',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+         'Programming Language :: Python :: 3.9',
+        f'Topic :: Multimedia :: Sound/Audio :: FL Studio'
+    ],
+    keywords=keyword,
+    packages=packages,
+    python_requires='>=3.6, <4',
+    package_dir={
+        '':'src'
+    },
+    package_data={
+        'data': [f'*.flp']
+    },
+    data_files=[('/usr/local/bin',scripts)]
+)
+
+
